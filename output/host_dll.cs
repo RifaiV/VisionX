dacabaaad1bbae3c9ca5aa49b4bd287b33f10693
Rifai <mohammad.al-rifaii@hotmail.com>
2025-01-08 15:51:42 +0200

    dumper

---
+++ output/host_dll.cs
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-01-08 01:41:57.579477700 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: host.dll
+    // Class count: 2
+    // Enum count: 0
+    public static class HostDll {
+        // Parent: CAnimScriptBase
+        // Field count: 1
+        public static class EmptyTestScript {
+            public const nint m_hTest = 0x10; // CAnimScriptParam<float32>
+        }
+        // Parent: None
+        // Field count: 1
+        public static class CAnimScriptBase {
+            public const nint m_bIsValid = 0x8; // bool
+        }
+    }
+}
