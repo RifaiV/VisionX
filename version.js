97c127e2515371ee5e47d292c3001df922c41d88
Rifai <106388043+RifaiV@users.noreply.github.com>
2025-03-02 13:22:56 +0200

    Update version.js

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 {
   "Version": "1.0.0.0",
-  "Checksum": "554fbc1bf8506adb5eee406a1466f468", // MD5 checksum of the DLL
-  "Size": 1523712 // File size in bytes (optional)
+  "Checksum": "6b6683e715e5a977ecefdcf2f3a3475f", // MD5 checksum of the DLL
+  "Size": 1527808 // File size in bytes (optional)
 }
