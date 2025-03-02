273bde4c4812118ae2243b7942cce870918a0604
Rifai <106388043+RifaiV@users.noreply.github.com>
2025-03-02 13:37:14 +0200

    c

--- vx.js
+++ vx.js
@@ -1,1 +1,1 @@
-{  "Version": "1.0.0.0",  "Checksum": "6b6683e715e5a977ecefdcf2f3a3475f", // MD5 checksum of the DLL  "Size": 1527808 // File size in bytes (optional)}
+{  "Version": "1.0.0.0",  "Checksum": "6b6683e715e5a977ecefdcf2f3a3475f", // MD5 checksum of the DLL  "Size": 1524736 // File size in bytes (optional)}
